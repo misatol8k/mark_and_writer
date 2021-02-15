5efa2a773a3d41ebe308fea6ac01f58f7c0eaa22
misatol8k <misatolkurihara@gmail.com>
2021-02-16 00:57:42 +0900

    マイグレーションを実施

--- db/migrate/20210215154026_create_works.rb
+++ db/migrate/20210215154026_create_works.rb
@@ -3,8 +3,7 @@ class CreateWorks < ActiveRecord::Migration[5.2]
     create_table :works do |t|
       t.string :title
       t.text :content, null: false
-      t.boolean :status, null: false, false
-      t.text :image
+      t.boolean :status, default: false
 
       t.timestamps
     end
