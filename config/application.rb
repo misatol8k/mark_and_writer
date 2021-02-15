14fbc45421ea71c4207528acd1e435fed85e2ac9
misatol8k <misatolkurihara@gmail.com>
2021-02-15 23:49:45 +0900

    helperファイルを作成しないよう設定

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module MarkAndWriter
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.generators do |g|
+      g.helper false
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
