a99aa7813ab5253f87812cac638c3ac67a3bbb58
misatol8k <misatolkurihara@gmail.com>
2021-02-16 16:32:06 +0900

    userモデルのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # :trackableのみ絡む追加済み、使用時に追加する
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :profile, length: { maximum: 1000 }
 end
