9271d086c9d31617f32a9337151ae12c4676b71b
misatol8k <misatolkurihara@gmail.com>
2021-02-16 15:52:48 +0900

    i18nの日本語化の設定をコメントアウト

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module MarkAndWriter
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
+    # config.i18n.default_locale = :ja
     config.generators do |g|
       g.helper false
       g.test_framework :rspec,
