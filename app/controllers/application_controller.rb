48a352c2b61287b6b8ff45a4be66d3cacc4e4990
misatol8k <misatolkurihara@gmail.com>
2021-02-16 15:13:06 +0900

    devise_controllerのみ許可するパラメータを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # devise_controllerのみ許可するパラメータを設定
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :profile])
+  end
 end
