30a81aefe3071a2348e862e23564eb886a8392eb
misatol8k <misatolkurihara@gmail.com>
2021-02-16 18:06:01 +0900

    image_uploaderでユーザー画像を登録できるよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :image_cache, :profile])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   # :trackableのみ絡む追加済み、使用時に追加する
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :image, ImageUploader
   validates :name, presence: true
   validates :profile, length: { maximum: 1000 }
 end
