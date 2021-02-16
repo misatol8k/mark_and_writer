85d1e298db04615ed893daf7fbfb4ef25d0c3276
misatol8k <misatolkurihara@gmail.com>
2021-02-16 15:10:52 +0900

    コメント追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :trackableのみ絡む追加済み、使用時に追加する
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
