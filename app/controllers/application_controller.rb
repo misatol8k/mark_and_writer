f382d8f404ee0e2e31f57157492131ef40ae88b0
misatol8k <misatolkurihara@gmail.com>
2021-02-16 23:28:29 +0900

    コメントを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # devise_controllerのみ許可するパラメータを設定
+  # allow params only devise_controller
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
