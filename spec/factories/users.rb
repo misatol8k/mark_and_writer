65beb09b42f8d29f81161decc19df0b773021a6f
misatol8k <misatolkurihara@gmail.com>
2021-02-22 00:27:01 +0900

    テスト用データを記載

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "user_admin" }
+    email { "test1@test.com" }
+    password { "password" }
+    profile { "My name is xxx. My favorite books are ... " }
+    image  { nil }
+    # admin { true }
+  end
+  factory :second_user, class: User do
+    name { "user_b" }
+    email { "test2@test.com" }
+    password { "password" }
+    profile { "My name is xxx. My favorite books are ... " }
+    image  { nil }
+    # admin { false }
   end
 end
--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -1,8 +1,31 @@
 FactoryBot.define do
   factory :work do
-    title { "MyString" }
-    content { "MyText" }
+    title { 'テスト1' }
+    content { 'テスト1の本文です。或曇つた冬の日暮である。私は横須賀発上り二等客車の隅に腰を下して、ぼんやり発車の笛を待つてゐた。とうに電燈のついた客車の中には、珍らしく私の外に一人も乗客はゐなかつた。' }
     status { false }
-    image { "MyText" }
+    association :user
+    # after(:create) do |work|
+    #   create(:labeling, work: work, label: create(:label))
+    #   create(:labeling, work: work, label: create(:second_label))
+    # end
+  end
+  factory :second_work, class: Work do
+    title { 'テスト2' }
+    content { 'テスト2の本文です。暮色を帯びた町はずれの踏切りと、小鳥のように声を挙げた三人の子供たちと、そうしてその上に乱落らんらくする鮮あざやかな蜜柑の色と――すべては汽車の窓の外に、瞬またたく暇もなく通り過ぎた。' }
+    status { false }
+    association :user
+    # after(:create) do |work|
+    #   create(:labeling, work: work, label: Label.second)
+    #   create(:labeling, work: work, label: create(:third_label))
+    # end
+  end
+  factory :third_work, class: Work do
+    title { 'テスト3' }
+    content { 'テスト3の本文です。私はこの時始めて、云いようのない疲労と倦怠とを、そうして又不可解な、下等な、退屈な人生を僅わずかに忘れる事が出来たのである。' }
+    status { false }
+    association :user
+    # after(:create) do |work|
+    #   create(:labeling, work: work, label: Label.last)
+    # end
   end
 end
