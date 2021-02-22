8a3f5e15bd3e597f8ae56a0414cd34c2911d9a28
misatol8k <misatolkurihara@gmail.com>
2021-02-22 17:33:38 +0900

    テストデータを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -15,4 +15,12 @@ FactoryBot.define do
     image  { nil }
     # admin { false }
   end
+  factory :third_user, class: User do
+    name { "user_c" }
+    email { "test3@test.com" }
+    password { "password" }
+    profile { "My name is xxx. My favorite books are ... " }
+    image  { nil }
+    # admin { false }
+  end
 end
