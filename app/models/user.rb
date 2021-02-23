d679d8155cf184ee2305688f03ac6c06b8652950
misatol8k <misatolkurihara@gmail.com>
2021-02-23 20:47:38 +0900

    relationships アソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :works, dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   validates :name, presence: true, length: { maximum: 100 }
   validates :profile, length: { maximum: 1000 }
   mount_uploader :image, ImageUploader
