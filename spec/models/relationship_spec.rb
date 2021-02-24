8e20cc22e42f0443f338ae59db6ebc8b4dde4c20
misatol8k <misatolkurihara@gmail.com>
2021-02-24 10:43:46 +0900

    Relationshipのテストを記載

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user_a = FactoryBot.create(:user)
+    @user_b = FactoryBot.create(:second_user)
+  end
+  describe 'バリデーションのテスト' do
+    # タイトルと本文があれば有効な状態であること
+    context 'フォロー・フォロワーidが設定されている場合' do
+      it 'is valid with a follower_id, followed_id' do
+        relationship = Relationship.new(
+          follower_id: @user_a.id,
+          followed_id: @user_b.id,
+          follower: @user_a,
+          followed: @user_b
+        )
+        relationship.valid?
+        expect(relationship).to be_valid
+      end
+    end
+  end
 end
