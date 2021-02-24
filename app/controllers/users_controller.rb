cce99a0b3ece41585a845db8eb2501ce29d16243
misatol8k <misatolkurihara@gmail.com>
2021-02-24 17:49:55 +0900

    フォロー一覧ページを設置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[index]
+
+  def index
+    @users = current_user.following.order(created_at: :desc)
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'works#index'
   resources :works
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
