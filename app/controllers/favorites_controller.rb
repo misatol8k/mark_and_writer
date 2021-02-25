f2b1995107e84accd2ed534bebd612dd55e47cba
misatol8k <misatolkurihara@gmail.com>
2021-02-25 20:02:13 +0900

    favorite 機能を追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,10 @@
 class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.create(work_id: params[:work_id])
+    redirect_to works_url, notice: "#{favorite.work.user.name}お気に入り登録しました"
+  end
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to works_url, notice: "#{favorite.work.user.name}お気に入り解除しました"
+  end
 end
