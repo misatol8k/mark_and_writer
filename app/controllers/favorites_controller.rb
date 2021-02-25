eb4409d19792f4fc5fe41304082bb6f6e7fa0570
misatol8k <misatolkurihara@gmail.com>
2021-02-26 01:50:20 +0900

    【途中】favorite 機能をパーシャルに移動

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,22 @@
 class FavoritesController < ApplicationController
+  # before_action :authenticate_user!
+  # respond_to? :js
+
   def create
     favorite = current_user.favorites.create(work_id: params[:work_id])
     redirect_to works_url, notice: "#{favorite.work.user.name}お気に入り登録しました"
+    # @favorite = current_user.favorites.create(work_id: params[:favorites][:work_id])
+    # favorite = current_user.favorites.build(user_id: @current_user.id, work_id: params[:favorites][:work_id])
+    # favorite.save!
+    # @work = Work.find_by(@work.id)
+    # current_user.favorite(@work)
   end
+
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to works_url, notice: "#{favorite.work.user.name}お気に入り解除しました"
+    # @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    # @work = current_user.favorite_works.find(params[:id])
+    # current_user.unfavorite(@work)
   end
 end
