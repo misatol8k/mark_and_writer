61fdb9a80bd7fc19a67a37279b17627fb7d861c8
misatol8k <misatolkurihara@gmail.com>
2021-03-07 00:27:47 +0900

    reactionsのアクションを記載

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,2 +1,11 @@
 class ReactionsController < ApplicationController
+  def create
+    reaction = current_user.reactions.create(work_id: params[:work_id])
+    redirect_to works_url, notice: "スタンプしました"
+  end
+
+  def destroy
+    reaction = current_user.reactions.find_by(id: params[:id]).destroy
+    redirect_to works_url, notice: "スタンプ削除しました"
+  end
 end
