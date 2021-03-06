aa2c5be9300615b41f1c848e3f5a2a8584f333fd
misatol8k <misatolkurihara@gmail.com>
2021-03-06 22:55:06 +0900

    stickers ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index]
   resources :relationships, only: [:create, :destroy]
   resources :favorites, only: [:create, :destroy]
+  resources :stickers, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
