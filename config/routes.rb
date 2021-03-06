6cd76b70c92a09fc6a226ab47e028e0f9e082ffd
misatol8k <misatolkurihara@gmail.com>
2021-03-07 00:25:28 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root 'tops#index'
   resources :works do
     resources :impressions
+    resources :reactions
   end
   resources :users, only: [:show, :index]
   resources :relationships, only: [:create, :destroy]
   resources :favorites, only: [:create, :destroy]
   resources :stickers, only: [:index, :create, :destroy]
-  resources :reactions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
