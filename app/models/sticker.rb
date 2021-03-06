cc85774f1943ba6f578eec4ad357ad715e68929a
misatol8k <misatolkurihara@gmail.com>
2021-03-06 23:05:24 +0900

    バリデーションとイメージアップローダの設定を追加

--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,3 +1,5 @@
 class Sticker < ApplicationRecord
   has_many :reactions, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 100 }
+  mount_uploader :image, ImageUploader
 end
