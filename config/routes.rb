1c16a16c73389a46ad279437e073a824cda0db38
misatol8k <misatolkurihara@gmail.com>
2021-03-06 22:55:22 +0900

    reactions ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :relationships, only: [:create, :destroy]
   resources :favorites, only: [:create, :destroy]
   resources :stickers, only: [:index, :create, :destroy]
+  resources :reactions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
