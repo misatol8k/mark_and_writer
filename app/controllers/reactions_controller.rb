3773890e1f7194bba4444b43949572be13019b86
misatol8k <misatolkurihara@gmail.com>
2021-03-09 01:36:31 +0900

    【未完了】コメントを追加

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,6 +1,7 @@
 class ReactionsController < ApplicationController
   def create
     reaction = current_user.reactions.create(work_id: params[:work_id], sticker_id: params[:sticker_id])
+    # work_idは詳細表示しているworkのid,user_idはcurrent_userのidを保存する
     redirect_to works_url, notice: "スタンプしました"
   end
 
