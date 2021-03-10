7d6d21d28da0551053e0e83974d6ce4fef6d5adf
misatol8k <misatolkurihara@gmail.com>
2021-03-10 18:54:54 +0900

    【未完了】パラメータを取得できない不具合の修正

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,12 +1,21 @@
 class ReactionsController < ApplicationController
   def create
-    reaction = current_user.reactions.create(work_id: params[:work_id], sticker_id: params[:sticker_id])
+    @reaction = current_user.reactions.build(reaction_params)
     # work_idは詳細表示しているworkのid,user_idはcurrent_userのidを保存する
+    binding.pry
+    @reaction.save
     redirect_to works_url, notice: "スタンプしました"
+    # binding.pry
   end
 
   def destroy
     reaction = current_user.reactions.find_by(id: params[:id]).destroy
     redirect_to works_url, notice: "スタンプ削除しました"
+  end
+
+  private
+  def reaction_params
+    # params.require(:reaction).permit(:sticker_id)
+    params.require(:reaction).permit(:sticker_id).merge(work_id: :work_id)
   end
 end
