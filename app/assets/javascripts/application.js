a040d05c2846c052136d6bc7530c54a5bccb0e52
misatol8k <misatolkurihara@gmail.com>
2021-09-08 14:13:02 +0900

    popperをyarn install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery.js
+//= require jquery/dist/jquery.js
+//= require popper.js/dist/popper.js
+//= require bootstrap/dist/js/bootstrap.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
